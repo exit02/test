4040b2995cb8ba40d5357a02daf5826a3b9d9eca
exit02 <95013304+exit02@users.noreply.github.com>
2021-12-08 09:34:28 +0900

    25.三項演算子

--- src/index.js
+++ src/index.js
@@ -156,7 +156,7 @@
 /**
  * map や filter を使った配列の処理
  */
-const nameArr = ["田中", "山田", "斎藤"];
+// const nameArr = ["田中", "山田", "斎藤"];
 // for (let index = 0; index < nameArr.length; index++) {
 //   console.log(`${index + 1}番目は、${nameArr[index]}さんです。`);
 // }
@@ -183,3 +183,21 @@ const nameArr = ["田中", "山田", "斎藤"];
 //   }
 // })
 // console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+// ある条件 ? ある条件がtrue : ある条件がfalse
+// const val1 = 1 > 0 ? `trueです。` : `falseです。`;
+// console.log(val1);
+
+// const num = "1300";
+// // console.log(num.toLocaleString());
+
+// const formattedNum = typeof num === `number` ? num.toLocaleString() : `数値を入力してください`;
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? `100を超えています!!` : `許容範囲内です`;
+// }
+// console.log(checkSum(50,40));
