386e69da597e0a5e0f77534283cb2b34331a4473
exit02 <95013304+exit02@users.noreply.github.com>
2021-12-09 09:37:28 +0900

    26.論理演算子の本当の意味を知ろう

--- src/index.js
+++ src/index.js
@@ -201,3 +201,26 @@
 //   return num1 + num2 > 100 ? `100を超えています!!` : `許容範囲内です`;
 // }
 // console.log(checkSum(50,40));
+
+/**
+ * 論理演算子の本当の意味を知ろう && ||
+ */
+// const flag1 = true;
+// const flag2 = true;
+
+// if (flag1 || flag2) {
+//   console.log("1か2はtrueです。");
+// }
+// if (flag1 && flag2) {
+//   console.log("1と2もtrueです。");
+// }
+
+// ||は左側がfalseなら右側を返す。
+// const num = 100;
+// const fee = num || "金額が未設定です。";
+// console.log(fee);
+
+// &&は左側がtrueなら右側を返す。
+const num2 = 100;
+const fee2 = num2 && "何か設定されました。";
+console.log(fee2);
